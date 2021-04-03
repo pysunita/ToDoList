dc83deaebbf8b47533cc1298aa2d8ea9e00b1c19
pysunita <sunitasahu.nl@gmail.com>
2021-04-03 20:51:08 +0200

    fetched data from database and displayed on browser

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from tasks.models import Task
 
 # Create your views here.
 def index(request):
-    return render(request,'tasks/lists.html')
+    tasks= Task.objects.all()
+    context={'tasks':tasks}
+    return render(request,'tasks/lists.html',context)
 
