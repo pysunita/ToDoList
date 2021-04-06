f7172e565edcce78b548857ee7798c223706a489
pysunita <sunitasahu.nl@gmail.com>
2021-04-06 20:27:35 +0200

    added list.html in template folder | created post request | created form

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from tasks.models import Task
+from tasks.forms import TaskForm
 
 # Create your views here.
 def index(request):
-    tasks= Task.objects.all()
-    context={'tasks':tasks}
-    return render(request,'tasks/lists.html',context)
-
+    tasks = Task.objects.all()
+    form=TaskForm()
+    if request.method == 'POST':
+        form=TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+    context={'tasks':tasks,'form':form}
+    return render(request,'tasks/list.html',context)
