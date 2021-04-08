ad22a3bc3018e696cca9043158dbd1634123bf4e
pysunita <sunitasahu.nl@gmail.com>
2021-04-08 20:27:27 +0200

    added delete.html and wrote views for delete function

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -15,6 +15,23 @@ def index(request):
     context={'tasks':tasks,'form':form}
     return render(request,'tasks/list.html',context)
 
-def updateTask(request,pk):
-    task=Task.objects.get(id=pk)
-    return render(request,'tasks/update_task.html')
+
+def updateTask(request, pk):
+	task = Task.objects.get(id=pk)
+
+	form = TaskForm(instance=task)
+
+	if request.method == 'POST':
+		form = TaskForm(request.POST, instance=task)
+		if form.is_valid():
+			form.save()
+			return redirect('/')
+
+	context = {'form':form}
+
+	return render(request, 'tasks/update_task.html', context)
+
+def deleteTask(request, pk):
+    item= Task.objects.get(id=pk)
+    context={'item':item}
+    return render(request, 'tasks/delete.html', context)
